c72f5ca3607a7e8adaccd65aba8f90ed19b33ad2
amaniommi <amaniamu3479@gmail.com>
2021-02-28 21:56:43 -0500

    DB url change

--- server/config/db.js
+++ server/config/db.js
@@ -1,2 +1,3 @@
-module.exports = { URI: "mongodb+srv://Amani:59W1g4eC9NPVrMDW@cluster0.wjucs.mongodb.net/businessContacts?retryWrites=true&w=majority" };
+module.exports = { URI: "mongodb://amani:59W1g4eC9NPVrMDW@ec2-52-3-252-225.compute-1.amazonaws.com/admin" };
+//module.exports = { URI: "mongodb+srv://Amani:59W1g4eC9NPVrMDW@cluster0.wjucs.mongodb.net/businessContacts?retryWrites=true&w=majority" };
 //module.exports = { URI: "mongodb://localhost/BContacts" }; //mongodb link to connecto for mongoose
