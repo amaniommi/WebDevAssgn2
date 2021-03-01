a2e39b4eba85383dd0692456df8ebd2c96e3f44a
amaniommi <amaniamu3479@gmail.com>
2021-03-01 06:30:04 +0530

    DB Url update

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,2 @@
-module.exports = { URI: "mongodb://localhost/BContacts" }; //mongodb link to connecto for mongoose
+module.exports = { URI: "mongodb+srv://Amani:59W1g4eC9NPVrMDW@cluster0.wjucs.mongodb.net/businessContacts?retryWrites=true&w=majority" };
+//module.exports = { URI: "mongodb://localhost/BContacts" }; //mongodb link to connecto for mongoose
