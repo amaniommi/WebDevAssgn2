9a1fa285e7213388cced703fe8913799fd90300c
amaniommi <amaniamu3479@gmail.com>
2021-03-01 22:37:24 -0500

    DB URI Update

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,3 @@
-module.exports = { URI: "mongodb://amani:59W1g4eC9NPVrMDW@ec2-52-3-252-225.compute-1.amazonaws.com/admin" };
+module.exports = { URI: "mongodb://amani:59W1g4eC9NPVrMDW@ec2-54-175-133-183.compute-1.amazonaws.com/admin" };
 //module.exports = { URI: "mongodb+srv://Amani:59W1g4eC9NPVrMDW@cluster0.wjucs.mongodb.net/businessContacts?retryWrites=true&w=majority" };
 //module.exports = { URI: "mongodb://localhost/BContacts" }; //mongodb link to connecto for mongoose
